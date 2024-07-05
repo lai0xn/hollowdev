1ce3f3a1155b9f7908060387f7618e9b952e94c7
edaywalid <becharwalid9@gmail.com>
2024-07-05 11:11:26 +0100

    added a function to check if a candidate exists by userid and electionid

--- voting-system/src/main/java/com/example/votingsystem/repository/CandidateRepository.java
+++ voting-system/src/main/java/com/example/votingsystem/repository/CandidateRepository.java
@@ -1,6 +1,8 @@
 package com.example.votingsystem.repository;
 
 import com.example.votingsystem.model.Candidate;
+import com.example.votingsystem.model.Election;
+import com.example.votingsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
 
+    boolean existsByUserIdAndElection(User user, Election election);
 }
