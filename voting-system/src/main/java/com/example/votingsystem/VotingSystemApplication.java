f06cba53e3cae99453ad305ad6efb6ff0489ec52
edaywalid <becharwalid9@gmail.com>
2024-07-05 09:10:00 +0100

    added the main function

---
+++ voting-system/src/main/java/com/example/votingsystem/VotingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.votingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSystemApplication.class, args);
+    }
+
+}
