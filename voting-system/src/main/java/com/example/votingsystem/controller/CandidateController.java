9dbf215c25e6caf4ed459561f6f417e117b649d9
edaywalid <becharwalid9@gmail.com>
2024-07-05 09:16:50 +0100

    added the candidate controller

--- voting-system/src/main/java/com/example/votingsystem/controller/CandidateController.java
+++ voting-system/src/main/java/com/example/votingsystem/controller/CandidateController.java
@@ -9,10 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/candidate/")
 @RequiredArgsConstructor
+
+/*
+    CandidateController is used to handle the candidate related requests
+    It is used to add a candidate, get all candidates, get candidate by id and get candidate votes
+ */
 public class CandidateController {
 
     private final CandidateService candidateService;
 
+
+    /*
+        addCandidate is used to add a candidate to the database
+        it takes a candidateRequest object as a parameter
+        and returns a response entity
+     */
     @PostMapping("/add")
     ResponseEntity<?> addCandidate(
             @RequestBody CandidateRequest candidateRequest
@@ -20,11 +31,20 @@ public class CandidateController {
         return candidateService.addCandidate(candidateRequest);
     }
 
+    /*
+        getAllCandidates is used to get all the candidates in the database
+        it returns a response entity
+     */
     @GetMapping("/all")
     ResponseEntity<?> getAllCandidates() {
         return candidateService.getAllCandidates();
     }
 
+    /*
+        getCandidateById is used to get a candidate by id
+        it takes the candidate id as a parameter
+        and returns a response entity
+     */
     @GetMapping("/{id}")
     ResponseEntity<?> getCandidateById(
             @PathVariable String id
@@ -32,6 +52,11 @@ public class CandidateController {
         return candidateService.getCandidateById(id);
     }
 
+    /*
+        getCandidateVotes is used to get the votes of a candidate
+        it takes the candidate id and the election id as parameters
+        and returns a response entity
+     */
     @GetMapping("/{id}/votes")
     ResponseEntity<?> getCandidateVotes(
             @PathVariable String id ,
