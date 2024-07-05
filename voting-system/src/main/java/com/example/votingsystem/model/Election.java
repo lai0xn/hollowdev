7b24c4a3a694b1a010f539dfedddac38e33ce3f7
edaywalid <becharwalid9@gmail.com>
2024-07-05 09:06:12 +0100

    added the election model

---
+++ voting-system/src/main/java/com/example/votingsystem/model/Election.java
@@ -0,0 +1,49 @@
+package com.example.votingsystem.model;
+
+import com.example.votingsystem.dto.ElectionDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/*
+    Election class is used to represent the election entity in the database
+    u can check the readme file #dbmodel to see the database model
+ */
+
+@Table(name = "elections")
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Election {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "electionid", nullable = false)
+    private UUID electionId;
+
+    private String electionName;
+    private Date electionStartDate;
+    private Date electionEndDate;
+
+    @OneToMany(mappedBy = "election", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Candidate> candidates;
+
+    @OneToMany(mappedBy = "election", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Vote> votes;
+
+    public ElectionDTO toDTO() {
+        return ElectionDTO.builder()
+                .electionId(electionId.toString())
+                .electionName(electionName)
+                .electionStartDate(electionStartDate.toString())
+                .electionEndDate(electionEndDate.toString())
+                .build();
+    }
+}
