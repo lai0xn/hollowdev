3c9fe3c8ec844bc860544b7aa210efb57f765391
edaywalid <becharwalid9@gmail.com>
2024-07-06 08:18:24 +0100

    added getElectionCandidates , deleteElection , getElectionVoteRanking + some update in the return message format and status handling

--- voting-system/src/main/java/com/example/votingsystem/service/ElectionService.java
+++ voting-system/src/main/java/com/example/votingsystem/service/ElectionService.java
@@ -68,13 +68,88 @@ public class ElectionService {
         and returns a response entity
      */
 
-    public ResponseEntity<?> getElectionById(String id) {
+    /*
+        getElectionCandidates is used to get the candidates for an election
+        it takes the election id as a parameter
+     */
+    public ResponseEntity<?> getElectionCandidates(String id) {
         try {
-            log.info("Fetching election by id: {}", id);
-            return ResponseEntity.ok().body(electionRepository.findById(UUID.fromString(id)).map(Election::toDTO));
+            log.info("Fetching election candidates by id: {}", id);
+            return ResponseEntity.ok().body(electionRepository.findById(UUID.fromString(id)).map(election -> election.getCandidates().stream().map(Candidate::toDTO)));
         } catch (Exception e) {
-            log.error("Failed to fetch election", e);
-            return ResponseEntity.badRequest().body("Failed to fetch election");
+            log.error("Failed to fetch election candidates", e);
+            return ResponseEntity.badRequest().body("Failed to fetch election candidates");
+        }
+    }
+    /*
+        getElectionVoteRanking is used to get the vote ranking for an election
+     */
+    public ResponseEntity<?> getElectionVoteRanking(String id) {
+        try {
+            log.info("Fetching election vote ranking by id: {}", id);
+            log.info("checking if election exists");
+            if (!electionRepository.existsById(UUID.fromString(id))) {
+                log.warn("Election not found");
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "Election not found");
+                    }
+                });
+            }
+            log.info("Fetching candidates for election id: {}", id);
+            List<Candidate> candidates= electionRepository.findById(UUID.fromString(id)).map(Election::getCandidates).orElse(null);
+            if(candidates == null) {
+                log.warn("No candidates found for election id: {}", id);
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "No candidates found for election id: " + id);
+                    }
+                });
+            }
+
+            log.info("Converting candidates to voteDTO");
+            List<VoteDTO> voteDTOS = new ArrayList<>(candidates.stream().map(candidate -> VoteDTO.builder()
+                    .voteCount(candidate.getVotes().size())
+                    .candidateFirstName(candidate.getUserId().getFirstname())
+                    .candidateLastName(candidate.getUserId().getLastname())
+                    .build()).toList());
+            // sort by vote count
+
+            log.info("Sorting candidates by vote count");
+            voteDTOS.sort((a, b) -> b.getVoteCount() - a.getVoteCount());
+            return ResponseEntity.ok().body(voteDTOS);
+        } catch (Exception e) {
+            log.error("Failed to fetch election vote ranking", e);
+            return ResponseEntity.badRequest().body("Failed to fetch election vote ranking");
+        }
+    }
+    /*
+        DeleteElection is used to delete an election
+        it takes the election id as a parameter
+     */
+    public ResponseEntity<?> deleteElection(String id) {
+        try {
+            log.info("Deleting election by id: {}", id);
+            UUID electionId = UUID.fromString(id);
+            log.info("checking if election exists");
+            if (!electionRepository.existsById(electionId)) {
+                log.warn("Election not found");
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "Election not found");
+                    }
+
+                });
+            }
+            electionRepository.deleteById(electionId);
+            return ResponseEntity.ok().body(new HashMap<>(
+                                                    1) {{
+                                                put("message", "Election deleted successfully");
+                                            }}
+            );
+        } catch (Exception e) {
+            log.error("Failed to delete election", e);
+            return ResponseEntity.badRequest().body("Failed to delete election");
         }
     }
 }
