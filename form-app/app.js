7fd8f221fae58232b0ac43a6d9efb1fa83750cb1
edaywalid <becharwalid9@gmail.com>
2024-07-06 18:06:37 +0100

    added app.js file the based server runner

---
+++ form-app/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+const port = 3000;
+
+require("dotenv").config();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Use auth routes
+app.use("/auth", authRoutes);
+
+// basic route to test the server
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
