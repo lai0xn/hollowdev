8caa64f1abcf4498c026fbb04efe83bd711453e3
edaywalid <becharwalid9@gmail.com>
2024-07-06 08:09:29 +0100

    updated the return format and the status codes

--- voting-system/src/main/java/com/example/votingsystem/service/UserService.java
+++ voting-system/src/main/java/com/example/votingsystem/service/UserService.java
@@ -9,13 +9,21 @@ import com.example.votingsystem.model.USERTYPE;
 import com.example.votingsystem.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -39,31 +47,36 @@ public class UserService {
         and returns a response entity
      */
 
-    public ResponseEntity<?> register(RegisterRequest registerRequest) {
+    public ResponseEntity<?> register(RegisterRequest registerRequest , USERTYPE usertype) {
         try {
-            log.info("Registering user with request: {}", registerRequest);
+            log.info("Registering user with request: {}", registerRequest.getUsername());
 
             log.info("Checking if username and email already exists");
             if (userRepository.existsByUsername(registerRequest.getUsername())) {
                 log.warn("Username is already taken!");
-                return ResponseEntity.badRequest().body("Error: Username is already taken!");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new HashMap<>(1){
+                    {
+                        put("message", "Username is already taken!");
+                    }
+                });
             }
             if (userRepository.existsByEmail(registerRequest.getEmail())) {
                 log.warn("Email is already taken!");
-                return ResponseEntity.badRequest().body("Error: Email is already taken!");
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new HashMap<>(1){
+                    {
+                        put("message", "Email is already taken!");
+                    }
+                });
             }
 
-            log.info("Creating user");
-            if (registerRequest.getUserType() == null) {
-                log.info("User type is null, setting to NORMAL");
-            }
+
             User user = User.builder()
                     .firstname(registerRequest.getFirstname())
                     .lastname(registerRequest.getLastname())
                     .username(registerRequest.getUsername())
                     .email(registerRequest.getEmail())
                     .password(passwordEncoder.encode(registerRequest.getPassword()))
-                    .usertype(registerRequest.getUserType() != null ? registerRequest.getUserType() : USERTYPE.NORMAL) // Use directly
+                    .usertype(usertype) // Use directly
                     .build();
 
             log.info("Saving user to database");
@@ -72,14 +85,19 @@ public class UserService {
             log.info("Generating token");
             String token = jwtService.generateToken(user);
 
-            return ResponseEntity.ok().body(
+            return ResponseEntity.status(HttpStatus.CREATED).body(
                     new HashMap<>(1) {{
                         put("message", "User registered successfully!");
                         put("token", token);
+                        put("role" , user.getUsertype());
                     }});
         } catch (Exception e) {
-            log.error("Failed to register user", e);
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            log.error("Failed to register user");
+            return ResponseEntity.internalServerError().body(new HashMap<>(1){
+                {
+                    put("message", "Failed to register user with error: " + e.getMessage());
+                }
+            });
         }
     }
 
@@ -94,7 +112,11 @@ public class UserService {
             User user = userRepository.findById(userId).orElse(null);
             if (user == null) {
                 log.warn("User not found for id: {}", userId);
-                return ResponseEntity.badRequest().body("User not found");
+                return ResponseEntity.badRequest().body(new HashMap<>(1){
+                    {
+                        put("message", "User not found");
+                    }
+                });
             }
             // TODO: Convert User to UserDTO
             log.info("Converting user to UserDTO");
@@ -102,8 +124,10 @@ public class UserService {
 
             return ResponseEntity.ok().body(userDTO);
         } catch (Exception e) {
-            log.error("Failed to fetch user", e);
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            log.error("Failed to fetch user");
+            return ResponseEntity.badRequest().body(new HashMap<>(1){{
+                put("message", "Failed to fetch user with error: " + e.getMessage());
+            }});
         }
     }
 
@@ -117,39 +141,49 @@ public class UserService {
             log.info("Fetching all users");
             return ResponseEntity.ok().body(userRepository.findAll().stream().map(UserDTO::fromUser));
         } catch (Exception e) {
-            log.error("Failed to fetch users", e);
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            log.error("Failed to fetch users");
+            return ResponseEntity.badRequest().body(new HashMap<>(1){{
+                put("message", "Failed to fetch users with error: " + e.getMessage());
+            }});
         }
     }
 
     public ResponseEntity<?> login(LoginRequest loginRequest) {
         try {
-            log.info("Logging in user with request: {}", loginRequest.getUsername());
-
-            log.info("Authenticating user");
-            authenticationManager.authenticate(
+            log.info("Authenticating user with request: {}", loginRequest.getUsername());
+            Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
             );
-
-            log.info("Fetching user");
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             User user = userRepository.findByUsername(loginRequest.getUsername()).orElse(null);
             if (user == null) {
                 log.warn("User not found for username: {}", loginRequest.getUsername());
                 return ResponseEntity.badRequest().body("User not found");
             }
-
-            String token = jwtService.generateToken(user);
-
-            return ResponseEntity.ok().body(
-                    new HashMap<>(1) {{
-                        put("message", "User logged in successfully!");
-                        put("token", token);
-                    }});
-        } catch (Exception e) {
-            log.error("Failed to login user");
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            return ResponseEntity.ok().body(new HashMap<>(1) {{
+                put("message", "User authenticated successfully");
+                put("token", jwtService.generateToken(user));
+                put("role", user.getUsertype());
+            }});
+        } catch (BadCredentialsException e) {
+            log.error("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new HashMap<>(1){
+                {
+                    put("message", e.getMessage());
+                }
+            });
+        } catch (AuthenticationException e) {
+            log.error("Failed to authenticate user");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new HashMap<>(1){
+                {
+                    put("message", e.getMessage());
+                }
+            });
         }
+
+
     }
+
     public String getAuthenticatedUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null) {
