b35b2463339d52f22c7d1b9935e14ff0f311667e
edaywalid <becharwalid9@gmail.com>
2024-07-06 08:07:42 +0100

    updated the return format and the status codes

--- voting-system/src/main/java/com/example/votingsystem/service/VotingService.java
+++ voting-system/src/main/java/com/example/votingsystem/service/VotingService.java
@@ -10,8 +10,10 @@ import com.example.votingsystem.repository.ElectionRepository;
 import com.example.votingsystem.repository.UserRepository;
 import com.example.votingsystem.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,7 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 
@@ -28,12 +31,11 @@ import java.util.UUID;
  */
 
 public class VotingService {
-
-    private static final Logger log = LoggerFactory.getLogger(VotingService.class);
     private final VoteRepository voteRepository;
     private final UserRepository userRepository;
     private final ElectionRepository electionRepository;
     private final CandidateRepository candidateRepository;
+    private final UserService userService;
 
 
     /*
@@ -44,44 +46,70 @@ public class VotingService {
     public ResponseEntity<?> vote(VoteRequest voteRequest) {
         try {
             log.info("Voting with request: {}", voteRequest);
-
             // check if user exists
-            log.info("Checking if user exists {} " ,voteRequest.getVoterId());
-            User user =  userRepository.findById(UUID.fromString(voteRequest.getVoterId())).orElse(null);
-            if(user == null) {
-                log.warn("User not found for id: {}", voteRequest.getVoterId());
-                return ResponseEntity.badRequest().body("User not found");
+            log.info("check if candidate exists");
+            Candidate candidate = candidateRepository.findById(UUID.fromString(voteRequest.getCandidateId())).orElse(null);
+            if (candidate == null) {
+                log.warn("Candidate not found for id: {}", voteRequest.getCandidateId());
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "Candidate not found");
+                    }
+                });
             }
 
-            log.info("Checking if election exists {} " ,voteRequest.getElectionId());
-            Election election = electionRepository.findById(UUID.fromString(voteRequest.getElectionId())).orElse(null);
-            if(election == null) {
-                log.warn("Election not found for id: {}", voteRequest.getElectionId());
-                return ResponseEntity.badRequest().body("Election not found");
+            log.info("get authenticated user id ");
+            String userId = userService.getAuthenticatedUserId();
+            log.info("Checking if user exists {} ", userId);
+            User user = userRepository.findById(UUID.fromString(userId)).orElse(null);
+            if (user == null) {
+                log.warn("User not found for id: {}", userId);
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "User not found");
+                    }
+                });
             }
+            Election election = electionRepository.findById(candidate.getElection().getElectionId()).orElse(null);
 
+            assert election != null;
             // check if election has ended
             log.info("Checking if election has ended");
-            if(election.getElectionEndDate().toInstant().isBefore(Instant.now())) {
+            if (election.getElectionEndDate().toInstant().isBefore(Instant.now())) {
                 log.warn("Election has ended");
-                return ResponseEntity.badRequest().body("Election has ended");
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "Election has ended");
+                    }
+
+                });
+            }
+            // check if the user already voted for a candidate in this election
+            log.info("Checking if user has already voted for this candidate in this election");
+            int countByUserIdAndElectionId = voteRepository.countByUserIdAndElectionId(UUID.fromString(userId), election.getElectionId());
+            if (countByUserIdAndElectionId > 0) {
+                log.warn("User has already voted for a candidate in this election");
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "You have already voted for a candidate in this election");
+                    }
+
+                });
             }
 
-            // check if candidate exists
-            log.info("Checking if candidate exists {} " ,voteRequest.getCandidateId());
-            Candidate candidate = candidateRepository.findById(UUID.fromString(voteRequest.getCandidateId())).orElse(null);
-            if(candidate == null) {
-                log.warn("Candidate not found for id: {}", voteRequest.getCandidateId());
-                return ResponseEntity.badRequest().body("Candidate not found");
-            }
             // count votes of a user for a candidate in an election
             log.info("Checking if user has already voted for this candidate in this election");
-            int count = voteRepository.countByUserIdAndCandidateId(UUID.fromString(voteRequest.getVoterId()), UUID.fromString(voteRequest.getElectionId()), UUID.fromString(voteRequest.getCandidateId()));
+            int countByUserIdAndCandidateId = voteRepository.countByUserIdAndCandidateId(UUID.fromString(userId), UUID.fromString(voteRequest.getCandidateId()));
 
-            if(count > 0) {
+            if (countByUserIdAndCandidateId > 0) {
                 log.warn("User has already voted for this candidate in this election");
-                return ResponseEntity.badRequest().body("You have already voted for this candidate in this election");
+                return ResponseEntity.badRequest().body(new HashMap<>(1) {
+                    {
+                        put("message", "You have already voted for this candidate in this election");
+                    }
+                });
             }
+
 
             // i assumed that a user can vote to multiple candidates in a single election
 
@@ -94,14 +122,21 @@ public class VotingService {
 
             log.info("Saving vote to database");
             voteRepository.save(vote);
-            return ResponseEntity.ok().body(new HashMap<>(1){
+            return ResponseEntity.ok().body(new HashMap<>(1) {
                 {
-                    put("message" , "Vote casted successfully");
+                    put("message", "Vote casted successfully");
                 }
             });
-        }catch (Exception e) {
+
+        } catch (Exception e) {
             log.error("Failed to cast vote", e);
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new HashMap<>(1) {
+                {
+                    put("message", "Failed to cast vote with error: " + e.getMessage());
+                }
+            });
         }
+
+
     }
 }
