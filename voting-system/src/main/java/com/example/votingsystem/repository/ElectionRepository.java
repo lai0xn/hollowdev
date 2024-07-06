e6797c68d590dca063f7664425fa11437764c325
edaywalid <becharwalid9@gmail.com>
2024-07-06 08:06:09 +0100

    added a query to  check if an election already exists with the same name and start date

--- voting-system/src/main/java/com/example/votingsystem/repository/ElectionRepository.java
+++ voting-system/src/main/java/com/example/votingsystem/repository/ElectionRepository.java
@@ -3,10 +3,17 @@ package com.example.votingsystem.repository;
 
 import com.example.votingsystem.model.Election;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Repository
 public interface ElectionRepository extends JpaRepository<Election, UUID> {
+    /*
+    * existsByElectionNameAndElectionStartDate is used to check if an election already exists with the same name and start date
+     */
+    @Query("SELECT CASE WHEN COUNT(e) > 0 THEN TRUE ELSE FALSE END FROM Election e WHERE e.electionName = ?1 AND e.electionStartDate = ?2")
+    boolean existsByElectionNameAndElectionStartDate(String electionName, Date electionStartDate);
 }
