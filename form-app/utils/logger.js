9b9d0ed832163fb43db956eed59471dd678fbc18
edaywalid <becharwalid9@gmail.com>
2024-07-07 07:04:54 +0100

    added logger.js file to use it in the logging operations

---
+++ form-app/utils/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, transports, format } = require('winston');
+
+const logger = createLogger(
+  {
+    level: 'info',
+    format: format.combine(
+      format.timestamp(),
+      format.json()
+    ),
+    transports: [
+      new transports.Console(),
+      new transports.File({ filename: 'error.log', level: 'error' }),
+      new transports.File({ filename: 'combined.log' })
+    ]
+  },
+);
+
+module.exports = logger;
