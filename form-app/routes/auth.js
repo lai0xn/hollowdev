20bddf3cab41416a99b078476a174e4b485928db
edaywalid <becharwalid9@gmail.com>
2024-07-07 07:05:24 +0100

    added logging in register route

--- form-app/routes/auth.js
+++ form-app/routes/auth.js
@@ -4,22 +4,43 @@ const User = require("../models/UserModel");
 const { default: mongoose } = require("mongoose");
 const router = express.Router();
 const UserModel = mongoose.model("User");
+const logger = require("../utils/logger");
+
 // Register route
 router.post("/register", async (req, res) => {
   try {
+    logger.info("Registering new user");
     const { username, email, password } = req.body;
-    const user = new User({ username, email, password });
+
+    logger.info("Checking for missing fields");
+    if (!username || !email || !password) {
+      return res.status(400).send({ error: "Missing required fields" });
+    }
+
+    logger.info("Creating new user");
+    logger.info("Checking if username or email is already taken");
     const existingUser = await UserModel.isUsernameTaken(username);
     if (existingUser) {
+      logger.error("Username is already taken");
       return res.status(400).send({ error: "Username is already taken" });
     }
     const existingEmail = await UserModel.isEmailTaken(email);
     if (existingEmail) {
+      logger.error("Email is already taken");
       return res.status(400).send({ error: "Email is already taken" });
     }
+    logger.info("Username and email are available");
+
+    logger.info("Saving new user to database");
+    const user = new User({ username, email, password });
     await user.save();
+    logger.info("User saved successfully");
+
+    logger.info("Generating auth token");
     const token = await user.generateAuthToken();
+    logger.info("Auth token generated");
     res.status(201).send({ user, token });
+    logger.info("User registered successfully");
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
