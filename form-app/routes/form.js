8bfe9581270219241cb9071f0cd0d1dcdf71f5ba
edaywalid <becharwalid9@gmail.com>
2024-07-07 11:52:46 +0100

    added a basic structure on how the routes are going to look like

--- form-app/routes/form.js
+++ form-app/routes/form.js
@@ -18,5 +18,43 @@ router.post('/add', authMiddleware, async (req, res) => {
   }
 });
 
+// Get all forms
+router.get('/forms', authMiddleware, async (req, res) => {
+  // get all forms of a user so based on the owner id we can get the forms
+});
+
+// Get a form responses
+router.get('/forms/:id/responses?type', authMiddleware, async (req, res) => {
+  // there are two types of form response :
+  //  1. response based on each question
+  //  2. response based on each user
+  //  so based on the type we can get the response
+  //  if type is question then we will get the response based on each question
+  //  if type is user then we will get the response based on each user
+  //  if type is not provided then we will get the response based on each question
+
+  const type = req.params.type;
+  if (type === 'question' || !type) {
+    // get the response based on each question
+  } else if (type === 'user') {
+    // get the response based on each user
+  }
+});
+
+// Get a form
+router.get('/forms/:id', authMiddleware, async (req, res) => {
+  // get a form based on the id
+});
+
+// Update a form
+router.patch('/forms/:id', authMiddleware, async (req, res) => { });
+
+// Delete a form
+router.delete('/forms/:id', authMiddleware, async (req, res) => { });
+
+// Add a response to a form
+// this is used when it is not needed to be authenticated to respond to a form
+router.post('/forms/:id/add-response', async (req, res) => { });
+
 
 module.exports = router;
