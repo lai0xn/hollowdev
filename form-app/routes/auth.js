b5b08721433b8772367f18a0b503c6ca855e83b3
edaywalid <becharwalid9@gmail.com>
2024-07-07 19:04:16 +0100

    added logging in login process

--- form-app/routes/auth.js
+++ form-app/routes/auth.js
@@ -49,11 +49,27 @@ router.post("/register", async (req, res) => {
 // Login route
 router.post("/login", async (req, res) => {
   try {
+    logger.info("Logging in user");
+
+    logger.info("Checking for missing fields");
     const { auth_identifier, password } = req.body;
+    if (!auth_identifier || !password) {
+      logger.error("Missing required");
+      return res.status(400).send({ error: "Missing required fields" });
+    }
+    logger.info("Required fields provided");
+
+    logger.info("Finding user by credentials");
     const user = await UserModel.findByCredentials(auth_identifier, password);
+    logger.info("User found");
+
+    logger.info("Generating auth token");
     const token = await user.generateAuthToken();
+    logger.info("Auth token generated");
     res.send({ user, token });
+    logger.info("User logged in successfully");
   } catch (error) {
+    logger.error(error.message);
     res.status(400).send({ error: error.message });
   }
 });
