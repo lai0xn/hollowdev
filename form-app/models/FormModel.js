080487b963ea3723b9e9ca5c7e0af1b7664b3c18
edaywalid <becharwalid9@gmail.com>
2024-07-07 07:06:31 +0100

    added Form model

---
+++ form-app/models/FormModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { v4: uuidv4 } = require("uuid");
+const FormField = require("./FormFieldModel");
+const FormSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      default: uuidv4,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    fields: [FormField.schema],
+  },
+  { timestamps: true },
+);
+
+const Form = mongoose.model("Form", FormSchema);
+module.exports = Form;
