3329971d2751bce4e05e5cf6b166d6473af0c0ae
edaywalid <becharwalid9@gmail.com>
2024-07-08 15:43:37 +0100

    removed the update form fields from the form patch request

--- form-app/routes/form.js
+++ form-app/routes/form.js
@@ -134,38 +134,6 @@ router.patch("/:id", authMiddleware, async (req, res) => {
     // Save updated form
     await form.save();
 
-    // If fields are updated, handle field updates separately
-    if (updates.fields) {
-      for (const fieldUpdate of updates.fields) {
-        if (fieldUpdate._id) {
-          // Update existing field
-          await FormField.updateOne(
-            { _id: fieldUpdate._id, formId: formId },
-            { $set: fieldUpdate }
-          );
-        } else {
-          // Add new field
-          const newField = new FormField({
-            ...fieldUpdate,
-            formId: formId,
-          });
-          await newField.save();
-          form.fields.push(newField._id);
-        }
-      }
-
-      // Remove fields that are not in the updates
-      const updatedFieldIds = updates.fields.map(field => field._id).filter(id => id);
-      await FormField.deleteMany({
-        formId: formId,
-        _id: { $nin: updatedFieldIds },
-      });
-
-      // Update form fields array
-      form.fields = updatedFieldIds;
-      await form.save();
-    }
-
     res.status(200).send(form);
   } catch (e) {
     res.status(400).json({ error: e.message });
